9924fbf532870b09778ac841939f1cf95b1d82c9
Ryan Brooks <ryanbrooksis@gmail.com>
2014-06-03 15:24:28 +0100

    Adding DynamicEquality mixin and blank test case

---
+++ src/main/groovy/uk/co/ryanbrooks/equalitymixin/DynamicEquality.groovy
@@ -0,0 +1,29 @@
+package uk.co.ryanbrooks.equalitymixin
+
+class DynamicEquality {
+	@Override
+	public boolean equals(Object obj){
+
+		// Bail out early if the object is null or it's not the same class
+		if(obj == null ){
+			return false
+		}
+		if(!(obj.class == this.class)) {
+			return false
+		}
+
+		// Are they references to the same object?
+		if(this.is(obj)){
+			return true
+		}
+
+		// We ignore property names prefixed with two underscores ('__')
+		// to prevent comparisons of meta-properties
+		def propertiesToCompare = this.metaClass.properties.findAll{ !(it.name =~ /^__/) }
+
+
+		// Find the first non-matching property
+		def inequality = propertiesToCompare.find { property -> obj[property.name] != this[property.name] }
+		return inequality == null
+	}
+}
---
+++ src/test/groovy/uk/co/ryanbrooks/equalitymixin/DynamicEqualitySpec.groovy
@@ -0,0 +1,7 @@
+package uk.co.ryanbrooks.equalitymixin
+
+import spock.lang.Specification
+
+class DynamicEqualitySpec extends Specification {
+
+}
